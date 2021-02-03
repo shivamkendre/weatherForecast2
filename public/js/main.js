38bed5b7546c88bb0fcc2e859e9054b6b6edff22
Shivam Balaji Kendre <shivamkendre78@gmail.com>
2021-02-03 14:57:01 +0530

    Project added

---
+++ public/js/main.js
@@ -0,0 +1,78 @@
+const submit_button = document.getElementById('submit')
+const output_status = document.getElementById('output-status')
+const output = document.getElementById('output')
+const day = document.getElementById('day')
+const date = document.getElementById('date')
+const status = document.getElementById('status')
+const set_date_and_time = ()=>{
+    const dayArray = [
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thurs",
+        "Fri",
+        "Sat",
+    ]
+    const monthArr = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "July",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+    ]
+    const data = new Date()
+    const today = dayArray[data.getDay()]
+    const today_date = data.getDate()
+    const month = monthArr[data.getMonth()]
+    day.innerHTML = today
+    date.innerHTML = `${today_date} ${month}`
+}
+const get_info = async()=>{
+    const city_name = document.getElementById('city-name').value
+    if(city_name === ""){
+        output_status.style.color = "red"
+        output_status.style.fontWeight = "bold"
+        output_status.innerHTML = "Please Enter the city name before search"
+    }
+    else{
+     let api = `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=e6778392aaeb5d71c756182bb63986e7`
+     try{
+        const response =  await fetch(api)
+        const data = await response.json()
+        const temp = (data.main.temp - 273.15).toFixed(2)
+        const city = data.name
+        const country = data.sys.country
+        const temp_status = data.weather[0].main
+        if(temp_status == "Clear"){
+            status.innerHTML = '<i class = "fas fa-sun" style = "color :  rgb(245, 245, 17)"></i>'
+        }else if(temp_status == "Clouds"){
+            status.innerHTML = '<i class = "fas fa-cloud" style = "color :  #f1f2f6"></i>'
+        }else if(temp_status == "Rain"){
+            status.innerHTML = '<i class = "fas fa-cloud-rain" style = "color :  #a4b0be"></i>'
+        }else if(temp_status == "Snow" || temp_status == "Mist" || temp_status == "Smoke"){
+            status.innerHTML = '<i class="fas fa-thermometer-empty"></i>'
+        }else{
+            status.innerHTML = '<i class = "fas fa-sun" style = "color :  rgb(245, 245, 17)"></i>'
+        }
+        const degree = '<sup style = "margin-top: -2px">O</sup>'
+        output.innerHTML = `${temp} ${degree} C`
+        output_status.innerHTML = `${city}, ${country}`
+        output_status.style.color = "white"
+        output_status.style.fontWeight = "100"
+     }catch{
+        output_status.style.color = "red"
+        output_status.style.fontWeight = "bold"
+        output_status.innerHTML = "Please Enter valid city name"
+     }
+    }
+}
+set_date_and_time()
+submit_button.addEventListener('click', get_info)
---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const app = express()
+const port = process.env.PORT || 8000
+const host = "localhost"
+const static_path = path.join(__dirname, "../public")
+const partial_path = path.join(__dirname, "../templates/partials")
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, "../templates/views"))
+hbs.registerPartials(partial_path)
+app.use(express.static(static_path))
+app.get('/', (req, res)=>{
+    res.status(200).render('index')
+})
+app.get('/weather', (req, res)=>{
+    res.render('weather', {
+        name : "shivam"
+    })
+})
+app.get('*', (req, res)=>{
+    res.render('404')
+})
+app.listen(port , host, ()=>{
+    console.log("Listening to port 8000")
+})
